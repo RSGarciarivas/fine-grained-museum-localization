d092d007350ddf179ed6cc0e881156849d6ba455
rsg2703 <rafael_sg@outlook.com>
2021-10-13 13:27:41 -0500

    Feature matching enhanced

--- utils.py
+++ utils.py
@@ -80,7 +80,75 @@ def get_img_features(img_name, dataset):
     
     return kp, des
 
+# Get "good" matches between 2 images using its SIFT descriptors
+def match_features(des_1, des_2, flann, L_ratio = 0.7):
+    
+    # Obtain KNN matches
+    matches = flann.knnMatch(des_1, des_2, k = 2)
+    
+    # List for storing good matches
+    good_matches = []
+    
+    # Get unique, good enough matches
+    for m, n in matches:
+        
+        # Check if it's a good enough match
+        if m.distance < L_ratio * n.distance:
+            
+            # Check if match is already present in good_matches
+            if m.trainIdx in [match.trainIdx for match in good_matches]:
+                # Get location of that match in list
+                index = [match.trainIdx for match in good_matches].index(m.trainIdx)
+                
+                # Substitute old match if this match is better
+                if m.distance < good_matches[index].distance:
+                    good_matches[index] = m
+                        
+            # If match is not yet present, add it
+            else:
+                good_matches.append(m)
+    
+    return good_matches
 
-#def get_best_match()
 
-# FUNCTION FOR MATCHING FEATURES WELL!!!
+def get_best_match(img, dataset):
+    
+    # Get SIFT features for image of interest
+    sift = cv.SIFT_create()
+    kp_query, des_query = sift.detectAndCompute(img, None)
+    
+    # Dictionary for storing best match so far
+    best_match = {'name': '', 'matches': 0, 'good_matches': [], 'keypoints': []}
+    
+    # Set up Flann based matcher
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+    
+    # Go through each image in the dataset
+    for i, img_name in enumerate(dataset):
+        
+        print(f'Image {i+1} of {len(dataset)}. Best match so far: {best_match["matches"]} points.')
+        
+        # Load its SIFT features
+        kp, des = get_img_features(img_name, dataset)
+        
+        # Check both images have descriptors
+        if (des is not None) and (des_query is not None):
+            # Check each image has more than 2 descriptors
+            if (len(des) > 2) and (len(des_query) > 2):
+                
+                # Get matching features
+                good_matches = match_features(des_query, des, flann)
+                
+                # Update best match so far if better match found
+                if len(good_matches) > best_match['matches']:
+                    best_match['name'] = img_name
+                    best_match['matches'] = len(good_matches)
+                    best_match['good_matches'] = good_matches
+                    best_match['keypoints'] = kp
+    
+    return best_match['name'], best_match['good_matches'], [kp_query, best_match['keypoints']]
+
+
